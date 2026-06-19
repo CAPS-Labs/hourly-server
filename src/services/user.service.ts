731db3dafc9d5ad0625210e6f5f6399819498ff2
Colin Alvis <colin.alvis@outlook.com>
2026-06-19 11:52:06 -0700

    Add query to check if user already exist

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,8 +1,26 @@
 import { CreateUserDTO, UpdateUserDTO } from "#models/user.model.js";
 import { pool } from "#config/db.config.js";
 
-
+interface UserRow {
+    id: number | string;
+    fname: string;
+    mname: string;
+    lname: string;
+    email: string;
+    isActive: boolean;
+    created_at: Date;
+}
 
 export class UserService {
+
+    async createUser(dto: CreateUserDTO): Promise<Omit<UserRow, 'isActive'>> {
+        //Check is user exists using email
+        const checkEmailQuery = 'SELECT id FROM users WHERE email = $1';
+        const existingUserRes = await pool.query(checkEmailQuery, [dto.email]);
+
+        if (existingUserRes.rowCount && existingUserRes.rowCount > 0) {
+            throw new Error('Email is already in use');
+        }
+    }
 
 }
